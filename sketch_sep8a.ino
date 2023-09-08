08ed0332f7bc7006db6d3688b66122aef8bdbbd9
Pedro <pedroluiz0201690@gmail.com>
2023-09-08 15:00:37 -0300

    Added method displayAlarmInfo, it's being tested on the loop mehtod

--- sketch_sep8a.ino
+++ sketch_sep8a.ino
@@ -6,7 +6,7 @@
 #define LCD_COLUMNS 16
 #define LCD_ROWS 2
 
-
+// Initializing LCD, address can change according to the arduino/display you are using.
 LiquidCrystal_I2C lcd(LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS);
 
 Alarm alarms[5];
@@ -21,11 +21,38 @@ void setup() {
 
 void loop() {
   delay(1000);
-
+  displayAlarmInfo();
+  delay(1000);
+  if(currentAlarmIndex < 4) {
+    currentAlarmIndex++;
+  } else {
+    currentAlarmIndex = 0;
+  }
+  delay(1000);
 }
 
 void initializeAlarms(){
   for(uint8_t i = 0; i < 5; i++) {
     alarms[i] = Alarm();
   }
+}
+
+void displayAlarmInfo() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Alarm ");
+  lcd.print(currentAlarmIndex + 1);
+
+  lcd.setCursor(0, 1);
+  lcd.print("Hour: ");
+  lcd.print(alarms[currentAlarmIndex].getFormattedHour());
+
+  lcd.setCursor(9, 1);
+  lcd.print("Min: ");
+  lcd.print(alarms[currentAlarmIndex].getFormattedMinute());
+
+}
+
+void handleUserInput() {
+
 }
