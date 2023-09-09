252c3c5b167bd054689db1c7e63bd0e825ed82e8
Pedro <pedroluiz0201690@gmail.com>
2023-09-08 22:29:27 -0300

    added display mode next Alarm And Time screen and portion configuration

--- sketch_sep8a.ino
+++ sketch_sep8a.ino
@@ -1,16 +1,17 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
-#include "alarm.cpp"
-
+#include "Alarm.cpp" 
 #define LCD_ADDRESS 0x3F
 #define LCD_COLUMNS 16
 #define LCD_ROWS 2
 
-// Initializing LCD, address can change according to the arduino/display you are using.
+// Initializing LCD, address can change according to the Arduino/display you are using.
 LiquidCrystal_I2C lcd(LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS);
 
 Alarm alarms[5];
 uint8_t currentAlarmIndex = 0;
+uint8_t currentModeIndex = 0; 
+uint8_t portionAmount = 0;    
 
 void setup() {
   lcd.init();
@@ -21,18 +22,17 @@ void setup() {
 
 void loop() {
   delay(1000);
-  displayAlarmInfo();
-  delay(1000);
-  if(currentAlarmIndex < 4) {
-    currentAlarmIndex++;
+  if (currentModeIndex < 2) {
+    currentModeIndex++;
   } else {
-    currentAlarmIndex = 0;
+    currentModeIndex = 0;
   }
+  handleDisplayMode();
   delay(1000);
 }
 
-void initializeAlarms(){
-  for(uint8_t i = 0; i < 5; i++) {
+void initializeAlarms() {
+  for (uint8_t i = 0; i < 5; i++) {
     alarms[i] = Alarm();
   }
 }
@@ -50,7 +50,33 @@ void displayAlarmInfo() {
   lcd.setCursor(9, 1);
   lcd.print("Min: ");
   lcd.print(alarms[currentAlarmIndex].getFormattedMinute());
+}
 
+void displayNextAlarmAndTime() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(F("Nxt Alarm:"));
+
+  lcd.setCursor(0, 1);
+  lcd.print(F("Time:"));
+}
+
+void displayPortionAmount() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(F("Portion size:"));
+  lcd.print(" ");
+  lcd.print(portionAmount);
+}
+
+void handleDisplayMode() {
+  if (currentModeIndex == 0) {
+    displayNextAlarmAndTime();
+  } else if (currentModeIndex == 1) {
+    displayAlarmInfo();
+  } else {
+    displayPortionAmount();
+  }
 }
 
 void handleUserInput() {
